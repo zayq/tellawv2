7a7f8e0f7fa399d2599da118e289db8764782d26
dezo07 <blaze.desautels@gmail.com>
2023-04-30 12:03:45 -0400

    trululuuuu

--- backend/dashboard/wallet.js
+++ backend/dashboard/wallet.js
@@ -397,3 +397,19 @@ function openSettingWindow(){
 }
 
 
+
+// TOOGLE DARK-WHITE MODE
+const toggleSwitch = document.querySelector('.switch');
+
+function switchTheme(e) {
+    if (!e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, true);
+
+
